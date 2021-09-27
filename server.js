99768d681ca58e44b3341162a4fb4d2a58908857
유수현 <rhan0871@naver.com>
2021-09-27 12:35:48 +0900

    html, css, express 서버 세팅

---
+++ server.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const path=require('path');
+
+const app=express();
+
+app.use("/static", express.static(path.resolve(__dirname,"frontend", "static")))
+
+//요청이 들어오면 기본적으로 index.html 파일을 응답으로 보내준다. 
+//싱글 페이지 애플리케이션은 html이 하나만 있고 javascript를 통해 그 위에 모든 것을 만드는 것.
+app.get("/*" , (req,res)=>{
+    res.sendFile(path.resolve("frontend", "index.html"));
+})
+
+app.listen(process.env.PORT || 5001,()=>{console.log('server is running...')});
+
+  
