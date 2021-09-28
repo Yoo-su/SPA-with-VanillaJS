9dffea5846f72a36699350644876bebbd7d1f185
유수현 <rhan0871@naver.com>
2021-09-28 15:18:46 +0900

    -

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,58 +1,63 @@
-import Dashboard from "./views/Dashboard.js";
+import Dashboard from './views/Dashboard.js'
 import Posts from "./views/Posts.js";
+import PostView from "./views/PostView.js";
 import Settings from "./views/Settings.js";
 
-const navigateTo=(url)=>{
-    history.pushState(null,null,url);
-    router();
-}
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
 
-//js로 구현한 router
-const router=async()=>{
-    //각각의 경로와 element를 그릴 함수를 가진다.
-    const routes=[
-        {path:"/", view:Dashboard},
-        {path:"/posts", view:Posts},
-        {path:"/settings", view:Settings},
-    ];
+const getParams = match => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
 
-    //현재 url 경로와 같으면 isMatch를 true로 설정한다. 
-    const potentialMatches=routes.map(route=>{
-        return {
-            route:route,
-            isMatch:location.pathname===route.path,
-        }
-    });
-
-    //현재 url과 매치되는 것을 찾는다.
-    let match=potentialMatches.find(potentialMatch=>potentialMatch.isMatch);
-
-    //매칭되는 url이 아닌경우 기본 경로로 이동시킨다.
-    if (!match){
-        match={
-            route:routes[0],
-            isMatch:true
-        }
-    }
-
-    //매칭되는 뷰 클래스 객체를 생성한다. 
-    const view=new match.route.view();
-
-    document.querySelector('#app').innerHTML=await view.getHtml();
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }));
 };
 
-//뒤로 가기 눌렀을 때 router함수 호출한다. 
-window.addEventListener('popstate',router);
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
 
-//DOM 컨텐츠가 브라우저에 로드 되었을 때 실행할 함수를 정의한다.
-document.addEventListener("DOMContentLoaded",()=>{
-    //a 태그를 작동하지 않게 한다. 싱글 페이지 어플리케이션 방식으로 이동하게 한다.
-    document.body.addEventListener('click',e=>{
-        if(e.target.matches("[data-link]")){
+const router = async () => {
+    const routes = [
+        { path: "/", view: Dashboard },
+        { path: "/posts", view: Posts },
+        { path: "/posts/:id", view: PostView },
+        { path: "/settings", view: Settings }
+    ];
+
+    // Test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            result: location.pathname.match(pathToRegex(route.path))
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+
+    if (!match) {
+        match = {
+            route: routes[0],
+            result: [location.pathname]
+        };
+    }
+
+    const view = new match.route.view(getParams(match));
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
             e.preventDefault();
             navigateTo(e.target.href);
         }
-    })
+    });
 
     router();
-})
+});
